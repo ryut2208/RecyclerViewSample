bc6286cf3ed7f211864f20821bbcd0b42ea61116
RyutaTanaka <ryut2208@gmail.com>
2018-08-02 17:37:28 +0900

    First Commit

---
+++ app/src/main/java/com/ryut2208/recyclerviewsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ryut2208.recyclerviewsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
